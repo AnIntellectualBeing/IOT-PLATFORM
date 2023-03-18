ce401507e9adcb7155690cfe862c1bf2141d4ae3
E-25 <111394690+E-25@users.noreply.github.com>
2023-03-18 06:36:37 -0700

    Create http.cpp

---
+++ http.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <curl/curl.h>
+
+int main() {
+    CURL* curl;
+    CURLcode res;
+
+    curl = curl_easy_init();
+
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, "http://www.example.com");
+        res = curl_easy_perform(curl);
+
+        if (res != CURLE_OK) {
+            std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
+        }
+
+        curl_easy_cleanup(curl);
+    }
+
+    return 0;
+}
